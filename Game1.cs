ab226e879ca3eadd2341e3f5da7ebfda2188da47
Christian Moyes <339463846@GoTVDSB.ca>
2024-10-10 09:07:24 -0400

    Made Grid

--- Game1.cs
+++ Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Monogame_Topic_3___Loops__Lists__and_Input
 {
@@ -24,6 +25,8 @@ namespace Monogame_Topic_3___Loops__Lists__and_Input
 
         Vector2 mowerSpeed;
 
+        List<Rectangle> grassTiles;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -39,6 +42,17 @@ namespace Monogame_Topic_3___Loops__Lists__and_Input
             _graphics.ApplyChanges();
 
             mowerRect = new Rectangle(100, 100, 30, 30);
+
+            grassTiles = new List<Rectangle>();
+
+            for (int x = 0; x < window.Width; x += 5)
+            {
+                for (int y = 0; y < window.Height; y += 5)
+                {
+                    grassTiles.Add(new Rectangle(x, y, 5, 5));
+                }
+            }
+
 
             base.Initialize();
         }
@@ -60,14 +74,65 @@ namespace Monogame_Topic_3___Loops__Lists__and_Input
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            keyboardState = Keyboard.GetState();
+
+            mowerSpeed = Vector2.Zero;
+
+            if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+            {
+                mowerSpeed.Y -= 1;
+
+                if (mowerRect.Y == 0)
+                {
+                    mowerSpeed.Y = 0;
+                }
+            }
+            if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+            {
+                mowerSpeed.Y += 1;
+
+                if (mowerRect.Bottom == window.Bottom)
+                {
+                    mowerSpeed.Y = 0;
+                }
+            }
+            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+            {
+                mowerSpeed.X -= 1;
+
+                if (mowerRect.X == 0)
+                {
+                    mowerSpeed.X = 0;
+                }
+            }
+            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+            {
+                mowerSpeed.X += 1;
+
+                if (mowerRect.Right == window.Right)
+                {
+                    mowerSpeed.X = 0;
+                }
+            }
+
+            if (mowerSpeed == Vector2.Zero)
+            {
+                mowerSoundInstance.Stop();
+            }
+            else
+            {
+                mowerSoundInstance.Play();
+            }
+
+
+            mowerRect.Offset(mowerSpeed);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LightGreen);
             _spriteBatch.Begin();
             _spriteBatch.Draw(mowerTexture, mowerRect, Color.White);
             _spriteBatch.End();

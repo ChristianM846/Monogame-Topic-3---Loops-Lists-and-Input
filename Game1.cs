28c7e3222ba23a06c4acc87c37dd6c6252fd5f03
Christian Moyes <339463846@GoTVDSB.ca>
2024-10-10 08:52:37 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Topic_3___Loops__Lists__and_Input
+{
+    //Christian Moyes
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        KeyboardState keyboardState;
+
+        Texture2D grassTexture;
+        Texture2D mowerTexture;
+
+        Rectangle window;
+        Rectangle mowerRect;
+
+        SoundEffect mowerSound;
+        SoundEffectInstance mowerSoundInstance;
+
+        Vector2 mowerSpeed;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            window = new Rectangle(0, 0, 600, 500);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            mowerRect = new Rectangle(100, 100, 30, 30);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            mowerTexture = Content.Load<Texture2D>("Images/mower");
+            grassTexture = Content.Load<Texture2D>("Images/long_grass");
+
+            mowerSound = Content.Load<SoundEffect>("Sounds/mower_sound");
+            mowerSoundInstance = mowerSound.CreateInstance();
+            mowerSoundInstance.IsLooped = true;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(mowerTexture, mowerRect, Color.White);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_Topic_3___Loops__Lists__and_Input.Game1();
+game.Run();
